820a324a1cb68c43652b7e1af7cfc985a99b70fc
um.hau <um.hau@outlook.com>
2020-11-21 10:11:15 -0500

    bug found: previous dev did not plan on 64bit arch

--- proxydhcpd/net.py
+++ proxydhcpd/net.py
@@ -2,6 +2,7 @@ import socket
 import fcntl
 import struct
 import array
+import platform
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -12,6 +13,18 @@ def get_ip_address(ifname):
     )[20:24])
 
 def all_interfaces():
+
+    arch = platform.architecture()[0]
+
+    if arch == '32bit':
+        magic1 = 32
+        magic2 = 32
+    elif arch == '64bit':
+        magic1 = 16
+        magic2 = 40
+    else:
+        raise OSError("Unknown architecture: %s" % arch)
+
     max_possible = 128
     bytes = max_possible * 32
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,7 +35,7 @@ def all_interfaces():
         struct.pack('iL', bytes, names.buffer_info()[0])
     ))[0]
     namestr = names.tostring()
-    return [namestr[i:i+32].split('\0', 1)[0] for i in range(0, outbytes, 32)]
+    return [namestr[i:i+magic1].split('\0', 1)[0] for i in range(0, outbytes, magic2)]
 
 def get_dev_name(ipaddr):
     for netdev in all_interfaces():
